d9ed392a939a13b47375cc26b4787672df3208bc
Matthew Williams <love.cosma@gmail.com>
2021-07-27 17:41:52 -0600

    commit tree 437c49bdde623acbd45654759fe7b669d6b3f08a

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,29 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(num)
+
+    counter = 1
+    multiples = [] 
+
+    while(counter < num)
+        if(counter % 3 == 0 || counter % 5 == 0)
+            multiples << counter
+        end 
+        counter += 1
+    end
+
+    return multiples
+
+end 
+
+def sum_multiples(num)
+    multiples = collect_multiples(num)
+    sum = 0
+
+    multiples.each do |i|
+        sum += i
+    end 
+
+    return sum 
+end 

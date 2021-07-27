e87b979deb0f1811ef6aadba32f4e0acbdf8d49a
Matthew Williams <love.cosma@gmail.com>
2021-07-27 17:41:56 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,40 @@
 # Enter your object-oriented solution here!
+
+
+class Multiples 
+
+    attr_accessor :num
+
+    def initialize(num)
+        @num = num
+    end 
+
+    def collect_multiples
+
+        counter = 1
+        multiples = [] 
+    
+        while(counter < @num)
+            if(counter % 3 == 0 || counter % 5 == 0)
+                multiples << counter
+            end 
+            counter += 1
+        end
+    
+        return multiples
+    
+    end 
+    
+    def sum_multiples
+        multiples = collect_multiples
+        sum = 0
+    
+        multiples.each do |i|
+            sum += i
+        end 
+    
+        return sum 
+    end 
+
+    
+end 
